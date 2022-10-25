aa2d9b32434740e5d8e1c147adc0b38dee35ce2e
Roman Romanovsky <roman.romanovsky.github@gmail.com>
2022-10-25 22:43:59 +0200

    Add React ESLint rules

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,5 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './components/App'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('app')!).render(<App />)
